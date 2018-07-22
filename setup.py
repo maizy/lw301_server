d0b4c348109bd5d2f69ed2a179b33d52f9458ec1
Nikita Kovaliov <nikita@maizy.ru>
2018-07-22 21:19:22 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='lw301_weather_server',
+    name='lw301-server',
     version='0.0.1',
     install_requires=['tornado>=5.1, <6.0'],
     tests_require=['nose', 'pycodestyle'],
     test_suite='nose.collector',
     scripts=['lw301_server'],
-    exclude=['lw301_server_tests/*'],
+    packages=find_packages(exclude=['lw301_server_tests']),
     description='Emulate servers for Oregon Scientific LW301',
     author='Nikita Kovalev',
     author_email='nikita@maizy.ru',
